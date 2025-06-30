483798a3d1dcd43855456befbaf9a5b16b19968c
Sanjay <sanjay4subbu@gmail.com>
2025-06-30 13:27:13 +0530

    changed to Local API

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   const handleGetReel = async()=>{
     try{
-      const response = await axios.post("https://instagram-video-downloader-o2ks.onrender.com",{
+      const response = await axios.post("http://localhost:3001/api/download",{
         url:url,}
       );
       setDownloadLink(response.data.downloadLink);
