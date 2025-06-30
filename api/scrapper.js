9048c375c1ebd909df3a604cf8fd295044dd1a26
Sanjay <sanjay4subbu@gmail.com>
2025-06-30 13:34:37 +0530

    added cloud port

--- api/scrapper.js
+++ api/scrapper.js
@@ -4,7 +4,12 @@ import { instagramGetUrl } from "instagram-url-direct";
 import cors from "cors";
 
 const app=express();
-const port=3001;
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
 app.use(express.json());
 app.use(cors());
 
